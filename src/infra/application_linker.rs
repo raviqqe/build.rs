310151d5345dd040f776ea60e7efbf0309a7c6c3
Yota Toyama <raviqqe@gmail.com>
2021-06-08 02:09:30 +0000

    Remove modules

--- src/infra/application_linker.rs
+++
@@ -1,9 +0,0 @@
-use super::FilePath;
-
-pub trait ApplicationLinker {
-    fn link(
-        &self,
-        object_file_paths: &[FilePath],
-        application_name: &str,
-    ) -> Result<(), Box<dyn std::error::Error>>;
-}
--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -1,17 +1,13 @@
-mod application_linker;
 mod external_package;
 mod external_package_downloader;
-mod ffi_package_initializer;
 mod file_path;
 mod file_path_displayer;
 mod file_system;
 mod logger;
 mod prelude_package_downloader;
 
-pub use application_linker::*;
 pub use external_package::*;
 pub use external_package_downloader::*;
-pub use ffi_package_initializer::*;
 pub use file_path::*;
 pub use file_path_displayer::*;
 pub use file_system::*;
